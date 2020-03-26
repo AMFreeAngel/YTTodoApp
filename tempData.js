a06c1b76e27f7c0b30ed1e3198326b92f118a965
AMFreeAngel <anna-mariya_v@ukr.net>
2020-03-26 23:46:23 +0200

    First attempt

---
+++ tempData.js
@@ -0,0 +1,62 @@
+// Firebase Emulation
+
+export default tempData = [
+   {
+       name: "Create smth",
+       color: "#24A6D9",
+       todos: [
+           {
+               title: "Make app",
+               completed: false
+           },
+           {
+               title: "Redoo",
+               completed: true
+           },
+           {
+               title: "Work hard",
+               completed: true
+           },
+           {
+               title: "What",
+               completed: false
+           }
+       ]
+    },
+   {
+        name: "Play games",
+        color: "#8022D9",
+        todos: [
+             {
+                 title: "Doom",
+                 completed: false
+             },
+             {
+                 title: "CS",
+                 completed: false
+             },
+             {
+                title: "DST",
+                completed: true
+            }
+       ]
+    },
+   {
+        name: "Lolkek",
+        color: "#DB5926",
+        todos: [
+             {
+                 title: "dfsfDoom",
+                 completed: false
+             },
+             {
+                 title: "CSsdfsdf",
+                 completed: false
+             },
+             {
+                title: "DSsdfsdfT",
+                completed: true
+            }
+       ]
+    }
+];
